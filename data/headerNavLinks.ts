00fe86575ff2b252edf7cd3fd50f2a43de527aea
hmzelidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-01-17 00:06:49 -0800

    init repo

--- data/headerNavLinks.ts
+++
@@ -1,9 +0,0 @@
-const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: '~blog' },
-  { href: '/tags', title: 'by topic~' },
-  { href: '/projects', title: '~projects' },
-  { href: '/whoami', title: 'who am i~' },
-]
-
-export default headerNavLinks

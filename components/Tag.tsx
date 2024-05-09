2bca1f9447f1f5fb01b68137163d9075ca08aa12
Hamza El Idrissi <hamza.ezzharelidrissi1@gmail.com>
2024-05-09 19:21:39 +0100

    edit tags style

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -8,7 +8,7 @@ const Tag = ({ text }: Props) => {
   return (
     <Link
       href={`/tags/${slug(text)}`}
-      className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:text-primary-400 dark:hover:text-primary-400"
+      className="mr-3 mt-1 rounded-full border border-primary-500 px-3 py-1 text-sm font-medium uppercase text-primary-500 hover:border-primary-600 hover:text-primary-600 dark:border-primary-400 dark:text-primary-400 dark:hover:border-primary-400 dark:hover:text-primary-400"
     >
       {text.split(' ').join('-')}
     </Link>

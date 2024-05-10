d45f7dd10b4a4a33b99e21757536e424d7d87102
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-05-10 13:41:21 +0100

    fix homepage styling

--- app/Main.tsx
+++ app/Main.tsx
@@ -11,30 +11,34 @@ export default function Home() {
       <h1 className="text-3xl font-bold dark:text-white lg:text-5xl">Hi, I am Hamza</h1>
       <p className="my-2 text-lg lg:my-4 lg:text-2xl">Software Developer</p>
       <p className="font-light lg:text-xl">
-        Read more
-        <Link className="ml-2 mr-2 font-normal text-black" href="/whoami">
-          <RoughNotation
-            show
-            type="highlight"
-            animationDelay={250}
-            animationDuration={2000}
-            color={aboutColor}
-          >
-            about me
-          </RoughNotation>
-        </Link>
+        <span className="text-nowrap">Read more</span>
+        <span className="text-nowrap">
+          <Link className="ml-2 mr-2 font-normal text-black" href="/whoami">
+            <RoughNotation
+              show
+              type="highlight"
+              animationDelay={250}
+              animationDuration={2000}
+              color={aboutColor}
+            >
+              about me
+            </RoughNotation>
+          </Link>
+        </span>
         or
-        <Link className="ml-2 font-normal text-black" href={`mailto:${siteMetadata.email}`}>
-          <RoughNotation
-            show
-            type="highlight"
-            animationDelay={250}
-            animationDuration={2000}
-            color={contactColor}
-          >
-            contact me
-          </RoughNotation>
-        </Link>
+        <span className="text-nowrap">
+          <Link className="ml-2 font-normal text-black" href={`mailto:${siteMetadata.email}`}>
+            <RoughNotation
+              show
+              type="highlight"
+              animationDelay={250}
+              animationDuration={2000}
+              color={contactColor}
+            >
+              contact me
+            </RoughNotation>
+          </Link>
+        </span>
       </p>
     </div>
   )

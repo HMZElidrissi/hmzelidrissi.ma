0d4c3054a855fd2aa1755c4d34722587c84a89dd
Hamza El Idrissi <hamza.ezzharelidrissi1@gmail.com>
2024-08-06 15:09:37 +0100

    blog: distributed systems example

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -47,18 +47,20 @@ const computedFields: ComputedFields = {
  */
 function createTagCount(allBlogs) {
   const tagCount: Record<string, number> = {}
-  allBlogs.forEach((file) => {
-    if (file.tags && (!isProduction || file.draft !== true)) {
-      file.tags.forEach((tag) => {
-        const formattedTag = GithubSlugger.slug(tag)
-        if (formattedTag in tagCount) {
-          tagCount[formattedTag] += 1
-        } else {
-          tagCount[formattedTag] = 1
-        }
-      })
-    }
-  })
+  allBlogs
+    .filter((p) => !p.draft)
+    .forEach((file) => {
+      if (file.tags && (!isProduction || file.draft !== true)) {
+        file.tags.forEach((tag) => {
+          const formattedTag = GithubSlugger.slug(tag)
+          if (formattedTag in tagCount) {
+            tagCount[formattedTag] += 1
+          } else {
+            tagCount[formattedTag] = 1
+          }
+        })
+      }
+    })
   writeFileSync('./app/tag-data.json', JSON.stringify(tagCount))
 }
 

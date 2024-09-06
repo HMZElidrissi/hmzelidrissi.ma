ecdcb6127410ce6ce0199386052cf011cd7deb1a
hmzelidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-09-06 08:54:06 +0100

    remove analytics ID

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -18,25 +18,25 @@ const siteMetadata = {
   linkedin: 'https://www.linkedin.com/in/hmzelidrissi',
   spotify: 'https://open.spotify.com/user/offnvr16bsi0bvnpkrel36eyi',
   locale: 'en-US',
-  analytics: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-    umamiAnalytics: {
-      // We use an env variable for this site to avoid other users cloning our analytics ID
-      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-    },
-    plausibleAnalytics: {
-      plausibleDataDomain: 'hmzelidrissi.ma', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    },
-    // simpleAnalytics: {},
-    // posthogAnalytics: {
-    //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    // },
-    googleAnalytics: {
-      googleAnalyticsId: 'G-1HCH705F2Z', // e.g. G-XXXXXXX
-    },
-  },
+  // analytics: {
+  //   // If you want to use an analytics provider you have to add it to the
+  //   // content security policy in the `next.config.js` file.
+  //   // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
+  //   umamiAnalytics: {
+  //     // We use an env variable for this site to avoid other users cloning our analytics ID
+  //     umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+  //   },
+  //   plausibleAnalytics: {
+  //     plausibleDataDomain: 'hmzelidrissi.ma', // e.g. tailwind-nextjs-starter-blog.vercel.app
+  //   },
+  //   // simpleAnalytics: {},
+  //   // posthogAnalytics: {
+  //   //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+  //   // },
+  //   // googleAnalytics: {
+  //   //   googleAnalyticsId: 'G-1HCH705F2Z', // e.g. G-XXXXXXX
+  //   // },
+  // },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection

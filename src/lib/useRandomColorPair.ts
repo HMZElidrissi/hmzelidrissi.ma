91d79e3f5479848fead6cbbadff5b9e905e87c63
hmzelidrissi <hamza.ezzharelidrissi1@gmail.com>
2025-08-18 17:03:43 +0100

    content: add author, sample blog post, socials; feat(lib): utilities for client and colors

---
+++ src/lib/useRandomColorPair.ts
@@ -0,0 +1,16 @@
+import { useMemo } from "react";
+
+export type Tuple<T> = [T, T];
+
+export function useRandomColorPair(): Tuple<string> {
+  const colors: Tuple<string>[] = [
+    ["#F5E1FF", "#CAF0F8"],
+    ["#EAF4F4", "#FFEDD8"],
+    ["#EDF8FF", "#EDE9F6"],
+    ["#EEEBFF", "#FFFAD4"],
+  ];
+
+  const random = Math.round(Math.random() * (colors.length - 1));
+
+  return useMemo(() => colors[random]!, [random]);
+}
